c065c0d6d99a5721ffeca6908a74602b9d155e7f
martinboykov <martinboykov@mail.bg>
2020-09-08 00:52:58 +0300

    test (tasks) tests for task service

--- client/src/app/core/services/tasks.service.spec.ts
+++ client/src/app/core/services/tasks.service.spec.ts
@@ -1,19 +1,105 @@
-import { TestBed } from '@angular/core/testing';
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 import { TasksService } from './tasks.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { Task } from 'src/app/tasks/models/task.model';
+import { Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
+const urlTasks = environment.apiUrl + '/tasks';
+const task: Task = {
+    id: '1',
+    title: 'test',
+    description: 'teset',
+};
+const tasks: Task[] = [
+    { id: '1', title: 'test', description: 'teset' },
+    { id: '2', title: 'test', description: 'teset' },
+];
 describe('TasksService', () => {
     let service: TasksService;
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
             providers: [TasksService],
         });
         service = TestBed.inject(TasksService);
+        httpClient = TestBed.inject(HttpClient);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
+    afterEach(() => {
+        httpTestingController.verify();
+    });
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+    describe('getAll', () => {
+        it('should add get all tesks', () => {
+            // make a fake request
+            const result = service.getAll();
+            expect(result).toBeInstanceOf(Subscription);
+
+            // expect "GET" request
+            const req = httpTestingController.expectOne(urlTasks);
+            expect(req.request.method).toEqual('GET');
+
+            req.flush(tasks);
+            expect(service.tasksSub.value.length).toBe(2);
+        });
+    });
+    describe('getById', () => {
+        it('should get the task', () => {
+            // make a fake request
+            const result = service.getById(task.id);
+            expect(result).toBeInstanceOf(Subscription);
+
+            // expect "GET" request
+            const req = httpTestingController.expectOne(
+                urlTasks + `/${task.id}`
+            );
+            expect(req.request.method).toEqual('GET');
+            req.flush(task);
+        });
+    });
+    describe('add', () => {
+        it('should make POST req', () => {
+            const result = service.add(task);
+            expect(result).toBeInstanceOf(Subscription);
+
+            const req = httpTestingController.expectOne(urlTasks);
+            expect(req.request.method).toEqual('POST');
+            req.flush(task);
+        });
+    });
+    describe('update', () => {
+        it('should make PATCH req', () => {
+            const result = service.update(task);
+            expect(result).toBeInstanceOf(Subscription);
+
+            const req = httpTestingController.expectOne(
+                urlTasks + `/${task.id}`
+            );
+            expect(req.request.method).toEqual('PATCH');
+            req.flush(task);
+        });
+    });
+    describe('delete', () => {
+        it('should make DELETE req', () => {
+            const result = service.delete(task.id);
+            expect(result).toBeInstanceOf(Subscription);
+
+            const req = httpTestingController.expectOne(
+                urlTasks + `/${task.id}`
+            );
+            expect(req.request.method).toEqual('DELETE');
+            req.flush(task);
+        });
     });
 });
--- client/src/app/core/services/tasks.service.ts
+++ client/src/app/core/services/tasks.service.ts
@@ -1,94 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Subject, of } from 'rxjs';
+import { Subject, of, BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map, concatMap } from 'rxjs/operators';
 import { Task } from 'src/app/tasks/models/task.model';
 
-const urlTasks = environment.apiUrl + '/tasks/';
+const urlTasks = environment.apiUrl + '/tasks';
 
 @Injectable({
     providedIn: 'root',
 })
 export class TasksService {
     tasks: Task[] = [];
-    tasksSub = new Subject<Task[]>();
+    tasksSub = new BehaviorSubject<Task[]>(this.tasks);
 
     constructor(private http: HttpClient) {}
 
     getAll() {
-        this.http
+        return this.http
             .get<Task[]>(urlTasks, {
                 params: new HttpParams(),
-                // .set('order', 'asc')
-                // .set('pageNumber', pageNumber.toString())
-                // .set('pageSize', pageSize.toString()),
             })
-            .pipe
-            // map((data) => {
-            //     console.log(data);
-            // }),
-            ()
-            .subscribe(
-                (tasks: any) => {
-                    console.log(tasks);
-                    this.tasksSub.next(tasks !== null ? tasks : []);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
+            .subscribe((tasks: any) => {
+                this.tasksSub.next(tasks !== null ? tasks : []);
+            });
     }
     getById(id: string) {
-        return this.http.get<Task[]>(urlTasks + id);
+        return this.http
+            .get<Task[]>(urlTasks + `/${id}`)
+            .subscribe((task) => {});
     }
     add(task: Task) {
         const newTask = {
             ...task,
         };
-        this.http
-            .post<Task>(urlTasks, newTask)
-            // .pipe(
-            //     concatMap((res) => {
-            //         this.getAll();
-            //         return of(res);
-            //     })
-            // )
-            .subscribe(
-                (response) => {
-                    console.log(response);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
+        return this.http.post<Task>(urlTasks, newTask).subscribe((response) => {
+            console.log(response);
+        });
     }
     update(task: Task) {
-        this.http.patch(urlTasks + task.id, task).subscribe(
-            (response) => {
+        return this.http
+            .patch(urlTasks + `/${task.id}`, task)
+            .subscribe((response) => {
                 console.log(response);
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
+            });
     }
     delete(id: string) {
-        this.http
-            .delete(urlTasks + id)
-            // .pipe(
-            //     concatMap((res) => {
-            //         this.getAll();
-            //         return of(res);
-            //     })
-            // )
-            .subscribe(
-                (response) => {
-                    console.log(response);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
+        return this.http.delete(urlTasks + `/${id}`).subscribe((response) => {
+            console.log(response);
+        });
     }
 }
